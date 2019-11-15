5ba036fa9836bf35c7e4503caa75b2564e023537
ashutoshkushwaha81 <106117013@nitt.edu>
2019-11-15 22:33:11 +0530

    server-sliding-window-protocol

--- sliding-window-client.py
+++ sliding-window-client.py
@@ -1,33 +1,36 @@
-import socket 
+import socket
 import time
+
 s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
 
-s.connect(('',12312))
+s.connect(('',8080))
 
-totalframe = 10
+while True :
+    try :
+        data = raw_input("Enter the frame size : ")
+        totalframe = int(data)
+        break
+    except :
+        print 'error !! , please try again!'
 
-s.send(str(totalframe))
-lauda =s.recv(382)
-print lauda
+print data
+s.sendall(data)
 
-totalack = 0
+time.sleep(1)
 
-while totalack < totalframe :
-    another = totalack + 1
-    s.send(str(another))
-    print '%s is ack.'%(totalack+1)
+ackframe = 0
+
+while ackframe < totalframe :
+    send_data = str(ackframe + 1)
+    print('%s frame is send '%send_data)
+    s.sendall(send_data)
+    recv_data = s.recv(4096)
+    if recv_data =='True':
+        ackframe = ackframe + 1
+        print '%s frame is ack. '%ackframe
+
+    else :
+        print 'time out , resend frame again ..'
     time.sleep(1)
-    totalack = totalack + 1
-
-
-
-    # data = s.recv(4096)
-
-    # newdata = int(data, 10)
-
-    # if newdata == 1:
-    #     print '%s is ack.'%totalack
-    #     totalack = totalack + 1
-    # else :
-    #     print '%s is not ack'%totalack
 s.close()
+
--- sliding-window-server.py
+++ sliding-window-server.py
@@ -1,37 +1,38 @@
-import socket
+import socket 
 import time
+import random
+
 s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
 
-s.bind(('',12312))
+s.bind(('',8080))
+s.listen(5)
 
-s.listen(10)
-
+print 'server is waiting for connections..'
 while True :
-    conn ,addr = s.accept()
+    conn, addr = s.accept()
 
-    print 'connection with the new client addr : (%s , %s)'%addr
+    print 'connection is estb. addr :(%s , %s) '%addr
 
-    data = conn.recv(4086)
-    conn.send("mil gaya")
+    data = conn.recv(4096)
+    totalframe = int(data)
 
-    # print data
-
-    newdata = int(data)
-
-    print newdata 
-    num = 0 
-    while num < newdata:
-        newdata1 = conn.recv(4096)
+    time.sleep(1)
+    ackframe = 0
+    while ackframe<totalframe:
+        data = conn.recv(4096)
+        framerecv = int(data)
         
-        print 'data is recv. ',newdata1
+        if random.randint(0,10000)%2:
+            print 'data frame is recv. with frame no. as %s'%framerecv
+            ackframe = ackframe + 1
+            conn.sendall('True')
+        else:
+            conn.sendall('False')
         time.sleep(1)
-
-        num = num + 1
-    s.close()
+    print 'closing the server..'
     break
+s.close()
 
-
-
-
+    
 
 
